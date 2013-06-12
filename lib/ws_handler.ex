53ee1f4ec759a5e56689078ea634ad92ac6139ed
Roman Heinrich <roman.heinrich@gmail.com>
2013-06-12 13:01:30 +0200

    Squashed 'github.com/sasa1977/elixir_websocket/' content from commit 8531259
    git-subtree-dir: github.com/sasa1977/elixir_websocket
    git-subtree-split: 8531259454ba39b980cde96d2b660e071748e80c

---
+++ lib/ws_handler.ex
@@ -0,0 +1,43 @@
+defmodule ElixirWebsocket.WsHandler do
+  def init(_, _, _) do
+    {:upgrade, :protocol, :cowboy_websocket}
+  end
+  
+  
+  def websocket_init(_, req, _) do
+    send_messages(self)
+    {:ok, req, nil}
+  end
+  
+  defp send_messages(process) do
+    Process.spawn(fn() ->
+      Enum.each(1..10, fn(index) ->
+        :timer.sleep(:timer.seconds(1))
+        process <- {:message, "generated message #{index}"}
+      end)
+    end)
+  end
+  
+  
+  def websocket_info({:message, msg}, req, state) do
+    {:reply, {:text, msg}, req, state}
+  end
+  
+  def websocket_info(_, req, state) do
+    {:ok, req, state}
+  end
+  
+  
+  def websocket_handle({:text, msg}, req, state) do
+    {:reply, {:text, "response to #{msg} from client"}, req, state}
+  end
+  
+  def websocket_handle(_, req, state) do
+    {:ok, req, state}
+  end
+  
+  
+  def websocket_terminate(_, _, _) do
+    :ok
+  end
+end
